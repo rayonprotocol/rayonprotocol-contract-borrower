10e38f0f34d45e6f588ddac5ffe07d984fca186d
knil nissim <knilnissim@gmail.com>
2018-10-02 17:08:37 +0900

    Add BorrowerApp contract

---
+++ contracts/BorrowerApp.sol
@@ -0,0 +1,75 @@
+pragma solidity ^0.4.23;
+
+import "../../rayonprotocol-contract-common/contracts/RayonBase.sol";
+
+contract BorrowerApp is RayonBase {
+    struct BorrowerAppEntry{
+        address id;
+        string name;
+        uint256 index;
+    }
+
+    mapping(address => BorrowerAppEntry) internal borrowerAppMap;
+    address[] internal borrowerAppList;
+
+    // constructor
+    constructor(uint16 version) RayonBase("BorrowerApp", version) public {}
+
+    // Event defination
+    event LogBorrowerAppAdded(address indexed id);
+    event LogBorrowerAppUpdated(address indexed id);
+
+    function add(address _id, string _name) public onlyOwner {
+        BorrowerAppEntry storage entry = borrowerAppMap[_id];
+        require(!_contains(entry), "borrower app already registered");
+
+        entry.id = _id;
+        entry.name = _name;
+        entry.index = borrowerAppList.push(_id) - 1;
+
+        emit LogBorrowerAppAdded(_id);
+    }
+
+    function get(address _id) public view returns (address, string){
+        BorrowerAppEntry storage entry = borrowerAppMap[_id];
+        require(_contains(entry), "borrower app not found");
+
+        return (entry.id, entry.name);
+    }
+
+    function getByIndex(uint256 _index) public view onlyOwner returns (address, string){
+        require(_isInRange(_index), "borrower app index out of range");
+
+        address id = borrowerAppList[_index];
+        return get(id);
+    }
+    
+    function getIds() public view onlyOwner returns (address[]){
+        return borrowerAppList;
+    }
+
+    function update(address _id, string _name) public onlyOwner {
+        BorrowerAppEntry storage entry = borrowerAppMap[_id];
+        require(bytes(_name).length > 0, "borrower app name cannot be null");
+        require(_contains(entry), "borrower app not found");
+        entry.name = _name;
+        emit LogBorrowerAppUpdated(_id);
+    }
+
+    function contains(address _id) public view returns (bool) {
+        BorrowerAppEntry storage entry = borrowerAppMap[_id];
+        return _contains(entry);
+    }
+
+    function size() public view onlyOwner returns (uint) {
+        return borrowerAppList.length;
+    }
+
+    function _isInRange(uint256 _index) private view returns (bool) {
+        return (_index >= 0) && (_index < borrowerAppList.length);
+    }
+
+    function _contains(BorrowerAppEntry memory _entry) private pure returns (bool){
+        return _entry.id != address(0);
+    }
+}
---
+++ test/BorrowerApp.test.js
@@ -0,0 +1,95 @@
+import eventsIn from './helpers/eventsIn';
+const BorrowerApp = artifacts.require('./BorrowerApp.sol');
+const BigNumber = web3.BigNumber;
+
+require('chai')
+  .use(require('chai-bignumber')(BigNumber))
+  .use(require('chai-as-promised'))
+  .should();
+
+const contractVersion = 1;
+
+contract('BorrowerApp', function (accounts) {
+  const [owner, nonOwner, someBorrowerAppAdress, otherBorrowerAppAdress] = accounts;
+  let borrowerApp;
+  const someBorrowerApp = {
+    id: someBorrowerAppAdress,
+    name: 'BORROWER1',
+  };
+  const otherBorrowerApp = {
+    id: otherBorrowerAppAdress,
+    name: 'BORROWER2',
+  };
+
+  beforeEach(async function () {
+    borrowerApp = await BorrowerApp.new(contractVersion, { from: owner });
+  });
+
+  describe('Register', async function () {
+    it('add borrower app', async function () {
+      await borrowerApp.add(someBorrowerApp.id, someBorrowerApp.name, { from: owner }).should.be.fulfilled;
+    });
+
+    it('emits an event after borrower app is successfuly added', async function () {
+      const events = await eventsIn(borrowerApp.add(someBorrowerApp.id, someBorrowerApp.name, { from: owner }));
+      events.should.deep.include({
+        name: 'LogBorrowerAppAdded',
+        args: { id: someBorrowerApp.id },
+      });
+    });
+
+    it('reverts on adding borrower app by non owner', async function () {
+      await borrowerApp.add(someBorrowerApp.id, someBorrowerApp.name, { from: nonOwner }).should.be.rejectedWith('revert');
+    });
+  });
+
+  describe('Retrieve', async function () {
+    it('gets registered borrower app with id', async function () {
+      await borrowerApp.add(someBorrowerApp.id, someBorrowerApp.name).should.be.fulfilled;
+      const [someBorrowerAppId, someBorrowerAppName] = await borrowerApp.get(someBorrowerApp.id);
+      someBorrowerAppId.should.be.equal(someBorrowerApp.id);
+      someBorrowerAppName.should.be.equal(someBorrowerApp.name);
+    });
+
+    it('gets registered borrower app with index', async function () {
+      await borrowerApp.add(someBorrowerApp.id, someBorrowerApp.name).should.be.fulfilled;
+      const [someBorrowerAppId, someBorrowerAppName] = await borrowerApp.getByIndex(0);
+      someBorrowerAppId.should.be.equal(someBorrowerApp.id);
+      someBorrowerAppName.should.be.equal(someBorrowerApp.name);
+    });
+
+    it('gets how many borrower apps are registered', async function () {
+      await borrowerApp.size().should.eventually.be.bignumber.equal(0);
+
+      // get size after two borrower apps are registered
+      const borrowerApps = [someBorrowerApp, otherBorrowerApp];
+      await Promise.all(borrowerApps.map(({ id, name }) => borrowerApp.add(id, name)));
+      await borrowerApp.size().should.eventually.be.bignumber.equal(borrowerApps.length);
+    });
+
+    it('reverts on getting unregistered borrower app', async function () {
+      await borrowerApp.get(someBorrowerApp.id).should.be.rejectedWith('borrower app not found');
+      await borrowerApp.getByIndex(0).should.be.rejectedWith('borrower app index out of range');
+    });
+
+    it('reverts on getting borrower app by non owner', async function () {
+      await borrowerApp.get(someBorrowerApp.id, { from: nonOwner }).should.be.rejectedWith('revert');
+      await borrowerApp.getByIndex(0, { from: nonOwner }).should.be.rejectedWith('revert');
+    });
+  });
+
+  describe('Modification', async function () {
+    it('updates name of the registered borrwer app', async function () {
+      await borrowerApp.add(someBorrowerApp.id, someBorrowerApp.name).should.be.fulfilled;
+      await borrowerApp.update(someBorrowerApp.id, someBorrowerApp.name).should.be.fulfilled;
+    });
+
+    it('reverts on updating unregistered borrower app', async function () {
+      await borrowerApp.update(someBorrowerApp.id, someBorrowerApp.name).should.be.rejectedWith('borrower app not found');
+    });
+
+    it('reverts on updating borrower app by non owner', async function () {
+      await borrowerApp.update(someBorrowerApp.id, someBorrowerApp.name, { from: nonOwner }).should.be.rejectedWith('revert');
+    });
+  });
+});
