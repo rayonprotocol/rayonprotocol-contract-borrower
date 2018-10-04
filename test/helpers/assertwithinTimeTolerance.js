1a71fa3d1cd6ed49097ee8663700625706a6d777
knil nissim <knilnissim@gmail.com>
2018-10-04 14:46:21 +0900

    Add and update test helper

---
+++ test/helpers/assertwithinTimeTolerance.js
@@ -0,0 +1,27 @@
+const BigNumber = web3.BigNumber;
+/**
+ * - A helper adding an assertion method `withinTimeTolerance`.
+ * - Usage of assertion
+ *  - actualTime.shoud.be.withinTimeTolerance(expectedTime); // with default(1 sec) tolerance
+ *  - actualTime.shoud.be.withinTimeTolerance(expectedTime, 2); // with 2 sec tolerance
+ * @param {*} chai
+ * @param {*} utils
+ */
+export default function assertwithinTimeTolerance (chai, utils) {
+  const Assertion = chai.Assertion;
+  Assertion.addMethod('withinTimeTolerance',
+    /**
+     * assert time for block within small tolerance
+     * @param expectedTime
+     * @param tolerance tolerance time in seconds (default: 1 second)
+     */
+    function (expected, tolerance = 1) {
+      const actual = this._obj;
+      this.assert(typeof actual === 'number' || actual instanceof BigNumber, 'actual #{act} must be a number or BigNumber instance');
+      this.assert(typeof expected === 'number' || expected instanceof BigNumber, 'expected #{exp} must be a number or BigNumber instance', null, expected);
+      const actualTime = Number(actual);
+      const expectedTime = Number(expected);
+      actualTime.should.be.within(expectedTime - tolerance, expectedTime + tolerance);
+    }
+  );
+};
--- test/helpers/eventsIn.js
+++ test/helpers/eventsIn.js
@@ -1,4 +1,10 @@
-export default async tx => {
-  const { logs } = await tx;
+/**
+ * extract events(name and args) from tx receipt
+ * @param {Promise} txPromise transaction promise
+ */
+const eventsIn = async txPromise => {
+  const { logs } = await txPromise;
   return logs.map(log => ({ name: log.event, args: log.args })); // as Array<{ name: string, args: object }>
 };
+
+export default eventsIn;
