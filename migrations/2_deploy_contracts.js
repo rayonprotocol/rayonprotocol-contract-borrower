e371d81ccccb7ad0773cd01fc3e5ed8e6e30d82a
knil nissim <knilnissim@gmail.com>
2018-12-14 14:40:53 +0900

    Add BorrowerScore to deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 const BorrowerApp = artifacts.require('BorrowerApp.sol');
 const Borrower = artifacts.require('Borrower.sol');
 const BorrowerMember = artifacts.require('BorrowerMember.sol');
+const BorrowerScore = artifacts.require('BorrowerScore.sol');
 
 module.exports = function (deployer, network, accounts) {
   const contractVersion = 1;
@@ -8,5 +9,6 @@ module.exports = function (deployer, network, accounts) {
     .then(() => deployer.deploy(BorrowerApp, contractVersion))
     .then(() => deployer.deploy(Borrower, contractVersion))
     .then(() => deployer.deploy(BorrowerMember, contractVersion))
+    .then(() => deployer.deploy(BorrowerScore, contractVersion))
     .catch(error => console.error({ error }));
 };
